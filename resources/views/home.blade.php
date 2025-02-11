36f565efd0080518959ebb2375ff6a1b63ff600a
yokdays <yokdays2000@gmail.com>
2025-02-12 00:16:55 +0700

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
                                 </li>
                                 <li class="nav-item">
                                     <a href="{{ route('orders.completed') }}"
-                                        class="btn btn-primary">ดูคำสั่งซื้อที่สำเร็จ</a>
+                                        class="btn btn-primary">ดูคำสั่งซื้อ</a>
                                 </li>
                             @endif
 
